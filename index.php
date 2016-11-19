037dc34859cdb56e631c55449227ddcfd517237f
Evgeny Padniuk <lord_rb@mail.ru>
2016-11-18 16:17:52 -0800

    disable cache

--- index.php
+++ index.php
@@ -6,9 +6,8 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('views');
 
 $twig = new Twig_Environment($loader, array(
-  'cache' => 'cache',
+  'cache' => false,
 ));
 
 echo $twig->render('index.html', array('name' => '', 'title' => 'Startpage'));
 
-?>
