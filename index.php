9f98a47083b97e5faf543138e20881bb75d7294a
Evgeny Padniuk <lord_rb@mail.ru>
2016-12-11 17:52:11 -0800

    Add samples

--- index.php
+++ index.php
@@ -3,11 +3,42 @@
 require_once('lib/Twig/Autoloader.php');
 Twig_Autoloader::register();
 
+function getListFiles(&$all_files, $folder = './views')
+{
+    $fp = opendir($folder);
+    while ($cv_file = readdir($fp)) {
+        if (is_file($folder . "/" . $cv_file)) {
+
+            $all_files[$folder][] = [
+                'path' => str_ireplace('./views/', '', $folder . "/" . $cv_file),
+                'file' => $cv_file,
+            ];
+
+        } elseif ($cv_file != "." && $cv_file != ".." && is_dir($folder . "/" . $cv_file)) {
+            getListFiles($all_files, $folder . "/" . $cv_file);
+        }
+    }
+    closedir($fp);
+}
+
+$all_files = [];
+getListFiles($all_files);
+
 $loader = new Twig_Loader_Filesystem('views');
 
 $twig = new Twig_Environment($loader, array(
   'cache' => false,
 ));
 
-echo $twig->render('index.html', array('name' => '', 'title' => 'Startpage'));
+if (isset($_REQUEST['path'])) {
+    echo $twig->render($_REQUEST['path'], array(
+        'samples'   => $all_files,
+    ));
+} else {
+    echo $twig->render('index.html.twig', array(
+        'name'      => '',
+        'title'     => 'Startpage',
+        'samples'   => $all_files,
+    ));
+}
 
