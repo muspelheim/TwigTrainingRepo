225345ed071964163a8eec8c55df5b47eba4f976
Evgeny Padniuk <lord_rb@mail.ru>
2016-12-25 06:49:49 -0800

    Add filters

--- index.php
+++ index.php
@@ -4,19 +4,20 @@ require_once('lib/Twig/Autoloader.php');
 require_once('SampleObject.php');
 Twig_Autoloader::register();
 
-function getListFiles(&$all_files, $folder = './views')
+function getListFiles(&$all_files, $folder = './views', &$depth = 0)
 {
+    $level = 0;
     $fp = opendir($folder);
     while ($cv_file = readdir($fp)) {
         if (is_file($folder . "/" . $cv_file)) {
-            //TODO add filter for html files
             $all_files[$folder][] = [
                 'path' => str_ireplace('./views/', '', $folder . "/" . $cv_file),
                 'file' => $cv_file,
             ];
 
-        } elseif ($cv_file != "." && $cv_file != ".." && is_dir($folder . "/" . $cv_file)) {
-            getListFiles($all_files, $folder . "/" . $cv_file);
+        } elseif ($cv_file != "." && $cv_file != ".." && is_dir($folder . "/" . $cv_file) && $depth < 2) {
+            $level++;
+            getListFiles($all_files, $folder . "/" . $cv_file, $level);
         }
     }
     closedir($fp);
@@ -24,6 +25,8 @@ function getListFiles(&$all_files, $folder = './views')
 
 $all_files = [];
 getListFiles($all_files);
+unset($all_files['./views']);
+
 
 $loader = new Twig_Loader_Filesystem('views');
 
